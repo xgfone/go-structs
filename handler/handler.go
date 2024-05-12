fe0cddd98c3d36bf4be227e47daf996ad2aeab80
xgfone <xgfone@126.com>
2024-05-12 19:38:21 +0800

    rename interface{} to any

--- handler/handler.go
+++ handler/handler.go
@@ -19,36 +19,36 @@ import "reflect"
 
 // Handler is an interface to handle the struct field.
 type Handler interface {
-	Parse(string) (interface{}, error) // Used to optimize: pre-parse and cache the tag value.
-	Run(ctx interface{}, rootStructValue, fieldValue reflect.Value, fieldType reflect.StructField, arg interface{}) error
+	Parse(string) (any, error) // Used to optimize: pre-parse and cache the tag value.
+	Run(ctx any, rootStructValue, fieldValue reflect.Value, fieldType reflect.StructField, arg any) error
 }
 
 // Parser is the function to pre-parse the field tag value.
-type Parser func(string) (interface{}, error)
+type Parser func(string) (any, error)
 
 // Runner is the function to handle the struct field.
-type Runner func(interface{}, reflect.Value, reflect.Value, reflect.StructField, interface{}) error
+type Runner func(ctx any, root reflect.Value, vf reflect.Value, sf reflect.StructField, arg any) error
 
 // SimpleRunner converts a simple function to Runner.
-func SimpleRunner(f func(field reflect.Value, arg interface{}) error) Runner {
-	return func(_ interface{}, _, vf reflect.Value, _ reflect.StructField, arg interface{}) error {
+func SimpleRunner(f func(field reflect.Value, arg any) error) Runner {
+	return func(_ any, _, vf reflect.Value, _ reflect.StructField, arg any) error {
 		return f(vf, arg)
 	}
 }
 
 // FieldRunner converts a field value function to Runner.
 func FieldRunner(f func(reflect.Value, reflect.StructField, any) error) Runner {
-	return func(_ interface{}, _, vf reflect.Value, sf reflect.StructField, arg interface{}) error {
+	return func(_ any, _, vf reflect.Value, sf reflect.StructField, arg any) error {
 		return f(vf, sf, arg)
 	}
 }
 
 // Parse implements the interface Handler, which does nothing
 // and returns the original string input as the parsed result.
-func (f Runner) Parse(s string) (interface{}, error) { return s, nil }
+func (f Runner) Parse(s string) (any, error) { return s, nil }
 
 // Run implements the interface Handler.
-func (f Runner) Run(ctx interface{}, r, v reflect.Value, t reflect.StructField, arg interface{}) error {
+func (f Runner) Run(ctx any, r, v reflect.Value, t reflect.StructField, arg any) error {
 	return f(ctx, r, v, t, arg)
 }
 
@@ -67,8 +67,8 @@ type handler struct {
 	run   Runner
 }
 
-func noopparse(s string) (interface{}, error)         { return s, nil }
-func (h handler) Parse(s string) (interface{}, error) { return h.parse(s) }
-func (h handler) Run(c interface{}, r, v reflect.Value, t reflect.StructField, a interface{}) error {
+func noopparse(s string) (any, error)         { return s, nil }
+func (h handler) Parse(s string) (any, error) { return h.parse(s) }
+func (h handler) Run(c any, r, v reflect.Value, t reflect.StructField, a any) error {
 	return h.run(c, r, v, t, a)
 }
